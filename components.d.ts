7c74bfb7f4cb9c7a5ea3ba4347e4550bc51c00b2
kkangda7 <hoon.kang@anex.co.jp>
2022-06-10 14:25:40 +0900

    add/vue-router

---
+++ components.d.ts
@@ -0,0 +1,10 @@
+// components.d.ts
+
+declare module '@vue/runtime-core' {
+  export interface GlobalComponents {
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
+  }
+}
+
+export {}
