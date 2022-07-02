e98dcecb53d94b0b7b37f033316413597eaaaa2b
kkangda7 <hoon.kang@anex.co.jp>
2022-07-02 14:37:42 +0900

    fix/json-server

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,15 +1,20 @@
-const posts = [
-	{ id: 1, title: '제목1', content: '내용1', createAt: '2020-01-01' },
-	{ id: 2, title: '제목2', content: '내용2', createAt: '2020-02-02' },
-	{ id: 3, title: '제목3', content: '내용3', createAt: '2020-03-03' },
-	{ id: 4, title: '제목4', content: '내용4', createAt: '2020-04-04' },
-	{ id: 5, title: '제목5', content: '내용5', createAt: '2020-05-05' },
-];
+import axios from 'axios';
 
 export function getPosts() {
-	return posts;
+	return axios.get('http://localhost:4000/posts');
+}
+export function getPostById(id) {
+	return axios.get(`http://localhost:4000/posts/${id}`);
 }
 
-export function getPostById(id) {
-	return posts.find(item => item.id === id);
+export function createPost(data) {
+	return axios.post('http://localhost:4000/posts', data);
+}
+
+export function updatePost(id, data) {
+	return axios.put(`http://localhost:4000/posts/${id}`, data);
+}
+
+export function deletePost(id) {
+	return axios.delete(`http://localhost:4000/posts/${id}`);
 }
